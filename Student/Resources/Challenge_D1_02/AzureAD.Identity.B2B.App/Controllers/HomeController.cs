a385b794184427882148279b4683a99a012405fb
fparpas <pparpas@hotmail.com>
2022-11-30 13:23:21 +0200

    Add app code in repo

---
+++ Student/Resources/Challenge_D1_02/AzureAD.Identity.B2B.App/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Graph;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using active_directory_aspnetcore_webapp_openidconnect_v2.Models;
+
+namespace active_directory_aspnetcore_webapp_openidconnect_v2.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly GraphServiceClient _graphServiceClient;
+
+        public HomeController(ILogger<HomeController> logger,
+                          GraphServiceClient graphServiceClient)
+        {
+             _logger = logger;
+            _graphServiceClient = graphServiceClient;
+       }
+
+        [AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
+        public async Task<IActionResult> Index()
+        {
+            var user = await _graphServiceClient.Me.Request().GetAsync();
+            ViewData["ApiResult"] = user.DisplayName;
+
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Student/Resources/Challenge_D1_02/AzureAD.Identity.B2B.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace active_directory_aspnetcore_webapp_openidconnect_v2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
